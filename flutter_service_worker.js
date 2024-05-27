4f6f6de273116729ce13c528e476e48b7a8017ae
anhdev99 <90453773+anhdev99@users.noreply.github.com>
2024-05-27 14:20:05 +0700

    [update]

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,12 +4,12 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "91e292636f187186a181538b45ca9c99",
-"index.html": "afbc1e01d6d7014ea5072ea9409a3c31",
-"/": "afbc1e01d6d7014ea5072ea9409a3c31",
+"index.html": "9fc01ef196d8cda31242ca4733ed74e4",
+"/": "9fc01ef196d8cda31242ca4733ed74e4",
 "main.dart.js": "cbd2920f4789a709bff889afcb1c83ad",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "background.jpg": "0360033988688274977ea85d226b4a92",
-"favicon.jpg": "e643943868185031f83f390b593a0367",
+"favicon.png": "aead319683a87296903b872de0709674",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
